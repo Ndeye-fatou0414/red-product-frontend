c0a04e15008faed54e88a23a1554806a9595f4ce
Ndeye-fatou0414 <fatoundieguene98@gmail.com>
2026-01-22 11:40:26 +0000

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
-  base: "//red-product-frontend",
+  base: "/", // ✅ site à la racine
 });
+
